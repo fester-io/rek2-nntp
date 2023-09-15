a6c7757e5568f57572e742e92e44f636f88f5680
rek2 <rek2@hispagatos.org>
2023-09-15 23:21:21 +0200

    more changes

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,5 @@
 use native_tls::TlsConnector;
-use std::error::Error;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
 
 pub enum AuthType {
@@ -8,58 +7,55 @@ pub enum AuthType {
     SslTls,
 }
 
+trait ReadWrite: Read + Write {}
+impl<T: Read + Write> ReadWrite for T {}
+
 pub fn authenticate(
     server: &str,
     port: u16,
     username: &str,
     password: &str,
     auth_type: AuthType,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), Box<dyn std::error::Error>> {
     let addr = format!("{}:{}", server, port);
 
-    // Establishing the connection based on the auth_type
-    let mut stream: Box<dyn Write + BufRead> = match auth_type {
+    match auth_type {
         AuthType::Plain => {
-            let tcp_stream = TcpStream::connect(addr)?;
-            Box::new(BufReader::new(tcp_stream))
+            let mut stream: Box<dyn Read + Write> = Box::new(TcpStream::connect(addr)?);
+            common_authenticate(&mut stream, username, password)
         }
         AuthType::SslTls => {
             let tcp_stream = TcpStream::connect(addr)?;
-            let connector = TlsConnector::new()?;
-            let tls_stream = connector.connect(server, tcp_stream)?;
-            Box::new(BufReader::new(tls_stream))
+            let connector = TlsConnector::new().map_err(|e| e.to_string())?;
+            let tls_stream = connector
+                .connect(server, tcp_stream)
+                .map_err(|e| e.to_string())?;
+            let mut stream: Box<dyn Read + Write> = Box::new(tls_stream);
+            common_authenticate(&mut stream, username, password)
         }
-    };
+    }
+}
 
-    // Send the AUTHINFO USER command
-    let user_command = format!("AUTHINFO USER {}\\r\\n", username);
+pub fn common_authenticate(
+    stream: &mut (dyn Read + Write),
+    username: &str,
+    password: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut reader = BufReader::new(stream);
+
+    let user_command = format!("AUTHINFO USER {}\r\n", username);
     stream.write_all(user_command.as_bytes())?;
     stream.flush()?;
 
-    // Read server response for AUTHINFO USER
     let mut user_response = String::new();
-    stream.read_line(&mut user_response)?;
-    if !user_response.starts_with("381") {
-        return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "AUTHINFO USER failed",
-        )));
-    }
+    reader.read_line(&mut user_response)?;
 
-    // Send the AUTHINFO PASS command
-    let pass_command = format!("AUTHINFO PASS {}\\r\\n", password);
+    let pass_command = format!("AUTHINFO PASS {}\r\n", password);
     stream.write_all(pass_command.as_bytes())?;
     stream.flush()?;
 
-    // Read server response for AUTHINFO PASS
     let mut pass_response = String::new();
-    stream.read_line(&mut pass_response)?;
-    if !pass_response.starts_with("281") {
-        return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "AUTHINFO PASS failed",
-        )));
-    }
+    reader.read_line(&mut pass_response)?;
 
     Ok(())
 }
