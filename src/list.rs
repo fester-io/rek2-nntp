f438169f998f7fcd4b4635ba6c0e4edc38d24c6e
Jan te Beest <jan.te.beest@dnb.no>
2025-05-23 11:25:15 +0200

    list: read lossy utf-8

--- src/list.rs
+++ src/list.rs
@@ -37,22 +37,29 @@ pub async fn list_newsgroups(
     let mut newsgroups = Vec::new();
 
     loop {
-        let mut line = String::new();
-        reader.read_line(&mut line).await?;
-
-        if line == ".\r\n" {
+        let mut buf = Vec::new();
+        let n = reader.read_until(b'\n', &mut buf).await?;
+        if n == 0 {
             break;
         }
 
-        let parts: Vec<&str> = line.trim().split(" ").collect();
+        // drop trailing CRLF or LF
+        if buf.ends_with(&[b'\n']) {
+            buf.pop();
+        }
+        if buf.ends_with(&[b'\r']) {
+            buf.pop();
+        }
+
+        let utf8 = String::from_utf8_lossy(&buf);
+        let parts: Vec<&str> = utf8.trim().split_whitespace().collect();
         if parts.len() >= 4 {
-            let newsgroup = Newsgroup {
-                name: parts[0].to_string(),
+            newsgroups.push(Newsgroup {
+                name: parts[0].to_owned(),
                 high: parts[1].parse()?,
                 low: parts[2].parse()?,
-                status: parts[3].to_string(),
-            };
-            newsgroups.push(newsgroup);
+                status: parts[3].to_owned(),
+            });
         }
     }
 
